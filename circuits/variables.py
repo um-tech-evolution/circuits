aa1942f30f43db3334767a72a2451fc6e0f115f7
George Lesica <george@lesica.com>
2014-07-11 09:30:10 -0600

    Add circuit evaluation function for testing.

--- circuits/variables.py
+++ circuits/variables.py
@@ -15,3 +15,18 @@ N = 16
 
 
 X = bddvars('x', N)
+
+
+def evaluated_at(circuit, bitstring):
+    '''
+    Evaluates the given circuit (function array) at the given bitstring and
+    returns the resulting bitstring. The input and output bitstrings will be
+    lists of `0` and `1`. If the input bitstring is shorter than `N`, it will
+    be padded to the right with zeroes.
+    '''
+    if len(bitstring) < N:
+        inbits = bitstring + [0] * (N - len(bitstring))
+    else:
+        inbits = bitstring
+
+    return [int(e) for e in circuit.vrestrict({X: inbits})]
